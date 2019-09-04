0ebe0a0e633f73bd13ffe5ab496e93dba2c13e7b
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-09-04 19:03:37 +0300

    Add: golang example (TODO) + iPieTiMe YAML

---
+++ history/2019/09/apptodo.go
@@ -0,0 +1,27 @@
+// 2019-09-04  1805
+// Wow use time lib
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Логи и желание привнести ясность в процесс обучения и разработки")
+
+	// 2.5 секунды не прокатило, нужны целые
+	time.Sleep(2 * time.Second)
+	fmt.Println("Я сегодня так и не искал не найденные ключи от Серпа[Моск-шоссе]")
+}
+
+// TODO - Ежедневно практиковать написание кода: Golang, Ruby, Web, Svelte.JS, NativeScript
+
+/* LOG
+	- https://golang.org/pkg/log/
+	- https://golangcode.com/sleeping-with-go/
+	- https://gist.github.com/KeyJoo/1f47ed5cc9c7b629c0c1182d34847974 git add -p is your friend
+
+  - At this repo git ask my Git-account credentials ((
+*/
