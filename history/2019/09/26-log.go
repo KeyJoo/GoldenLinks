81a75fa28c82b70dcd03290f4709c9e31a5fe73c
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-09-26 21:49:42 +0300

    Add: Log: Saved Audio-books to @Cloud.mail.ru & Nota-Rius - 1.000 Rub

---
+++ history/2019/09/26-log.go
@@ -0,0 +1,24 @@
+// 2019-09-26  2138
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Нота-Риус, 2 часа до 17-ти")
+	fmt.Println("Потом МФЦ, справка: выписка из домовой книги")
+}
+
+// TODO - Ежедневно практиковать написание кода: Golang, Ruby, Web, Svelte.JS, NativeScript
+// Example of "Michael Lomonosov - way of study."
+
+/* LOG
+	- https://cloud.mail.ru/home/Audio-Books/
+	- https://www.lpi.org/our-certifications/exam-701-objectives
+	- https://habr.com/ru/post/126849/ < 22 августа 2011 в 20:25
+		Кукушка на bash своими руками
+Prometheus:
+	- http://onreader.mdl.ru/PrometheusUpAndRunning/content/Ch01.html
+
+  - Resave repo by SSH
+*/
