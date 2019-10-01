d2b0a4c1b5a89f4ce85add21206a51fb4a857620
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-10-01 05:46:55 +0300

    Add: :dove: PDF - Sabbath School commentary (For teachers) Download link

---
+++ history/2019/09/30-log.go
@@ -0,0 +1,25 @@
+// 2019-09-30  22138
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Субботняя Школа, 4й квартал 2019. Книги Ездры и Неемии")
+	fmt.Println("Сылки на скачивание пособия с комментариями")
+}
+
+// TODO - Ежедневно практиковать написание кода & git + GitLab
+
+/* LOG
+SabbathSchool:
+- "http://adventistfiles.net/documents/ss/2019/4/adult/pdf/Книги Ездры и Неемии. Пособие с комментариями.pdf"
+*/
+
+// PROBLEMS
+/*
+IDE Messages:
+main redeclared in this block
+	previous declaration at ./26-log.go:7:6
+
+*/
